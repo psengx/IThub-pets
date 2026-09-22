2243a8a0b6778589cff82e4e1c879db8ce7dbb21
psengx <psengx@gmail.com>
2026-09-22 19:41:46 +0300

    id changed

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const soundButtons = document.querySelectorAll(".btn");
 
 const audios = {
-    1: new Audio("sounds/"),
-    2: new Audio("sounds/Zosya.mp3"),
-    3: new Audio("sounds/Ozzy.mp3"),
-    4: new Audio("sounds/")
+    "Mirana": new Audio("./sounds/"),
+    "Zosya": new Audio("./sounds/Zosya.mp3"),
+    "Ozzy": new Audio("./sounds/Ozzy.mp3"),
 };
 
 soundButtons.forEach(function(button) {
