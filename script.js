e2f640f6140b90e9b773423630a2d2c70a3df945
psengx <psengx@gmail.com>
2026-09-22 19:33:57 +0300

    file names updated

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const soundButtons = document.querySelectorAll(".btn");
 
 const audios = {
-    1: new Audio("/sounds/"),
-    2: new Audio("/sounds/Зося.mp3"),
-    3: new Audio("/sounds/Оззи.mp3"),
-    4: new Audio("/sounds/")
+    1: new Audio("sounds/"),
+    2: new Audio("sounds/Zosya.mp3"),
+    3: new Audio("sounds/Ozzy.mp3"),
+    4: new Audio("sounds/")
 };
 
 soundButtons.forEach(function(button) {
